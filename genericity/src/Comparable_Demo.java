3ae6bd74ef38233ab06f85b64c32eedb2ef1c9b2
lixingze <1873798269@qq.com>
2020-10-09 19:58:36 +0800

    comparable的学习

---
+++ genericity/src/Comparable_Demo.java
@@ -0,0 +1,11 @@
+/**
+ * @program: OJhomework
+ * @description Comparable的学习
+ * @author: 李星泽
+ * @create: 2020-10-09 19:57
+ **/
+public class Comparable_Demo {
+    public static void main(String[] args) {
+
+    }
+}
